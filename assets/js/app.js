2b81a3a666b0865f8351f2272115d0268c01cfca
Cole Furfaro-Strode <cfurfarostrode@Coles-MacBook-Pro.local>
2015-11-17 10:33:49 -0500

    lower casing commands and punctuation

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,18 +19,18 @@ $(document).ready(function() {
 
   var responses = [
     'What are you worried about?',
-    'Yeah you don\'t have to worry about that',
-    'If it were me, I wouldn\'t even worry about that',
+    'Yeah you don\'t have to worry about that.',
+    'If it were me, I wouldn\'t even worry about that.',
     'What? What are you like crazy or something? Don\'t even worry about it!',
     'That\'s uh, I mean, that\'s not even a thing, so don\'t worry about it alright.',
     'So uh, yeah, I mean, I don\'t know. Just like don\'t worry about it ok?',
     'Oh geez, that\'s what you\'re worried about? Just let that one go.',
-    'Ok, that sounds like, I mean I don\'t want to alarm you, but you might want to see a doctor about that'
+    'Ok, that sounds like, I mean I don\'t want to alarm you, but you might want to see a doctor about that.'
   ];
 
   function interpreter(command, terminal) {
     var dontWorry = _.random(1, 10) < 4;
-    command = _.trim(command);
+    command = _.trim(command).toLowerCase();
 
     if (command === 'help') {
       help(terminal);
