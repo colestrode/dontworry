472ee55c4865cc00c7b2cf7c0b724a61a0aa7edb
Cole Furfaro-Strode <cfurfarostrode@Coles-MacBook-Pro.local>
2015-11-17 09:38:22 -0500

    new responses, better sizing

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,11 +10,39 @@ $(document).ready(function() {
     '|__/|__/\\____/_/  /_/  /_/\\___/\\__,_/  /_/  |_/_.___/\\____/\\__,_/\\__/(_)   \n\n';
 
   var config = {
-    greetings: greetings
+    greetings: greetings,
+    prompt: 'Mortys-Macbook ~/private/dontlook> ',
+    onClear: function(term) {
+      term.echo(greetings);
+    }
   };
 
+  var responses = [
+    'What are you worried about?',
+    'Yeah you don\'t have to worry about that',
+    'If it were me, I wouldn\'t even worry about that',
+    'What? What are you like crazy or something? Don\'t even worry about it!',
+    'That\'s uh, I mean, that\'s not even a thing, so don\'t worry about it alright.',
+    'Ok, that sounds like, I mean I don\'t want to alarm you, but you might want to see a doctor about that'
+  ];
+
   function interpreter(command, terminal) {
-    terminal.echo('Don\'t even worry about it.');
+    var dontWorry = _.random(1, 10) < 4;
+    command = _.trim(command);
+
+    if (command === 'help') {
+      help(terminal);
+    } else if(dontWorry) {
+      terminal.echo('Don\'t even worry about it.');
+    } else {
+      terminal.echo(_.sample(responses));
+    }
+
+    terminal.echo('');
+  }
+
+  function help(terminal) {
+    terminal.echo('Just tell me what you\'re worried about. I\'m here to help.');
   }
 
   $('#terminal').terminal(interpreter, config);
